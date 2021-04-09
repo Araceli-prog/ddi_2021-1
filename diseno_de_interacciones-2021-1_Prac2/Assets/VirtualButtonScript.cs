5b061c0575b3ff5a5bc425dfef2abca8250a080f
Araceli Luevano <72630838+Araceli-prog@users.noreply.github.com>
2021-04-09 12:58:47 -0700

    Prac6-y-62

---
+++ diseno_de_interacciones-2021-1_Prac2/Assets/VirtualButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    private VirtualButtonBehaviour virtualButton;
+    public Animator animcontroller;
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Se presionó");
+        animcontroller.SetBool("bailar", true);
+    } 
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("Se levantó");
+        animcontroller.SetBool("bailar", false);
+    }
+
+    void Awake()
+    {
+        virtualButton = GetComponent<VirtualButtonBehaviour>();
+    }
+
+    void Start()
+    {
+        if (virtualButton != null)
+        {
+            virtualButton.RegisterEventHandler(this);
+        }
+    }
+}
